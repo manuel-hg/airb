895a0300c02cd7f03ed70dabfc50ebe943f1f779
manuel-hg <manuel_hg_1@outlook.com>
2023-06-14 23:05:23 -0600

    update

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,22 +1,23 @@
+import '../globals.css'
 import Image from "next/image";
 
 
 
 function Header() {
     return(
-        <header className="sticky top-0 z-50">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 xl:px-10">
             {/* Left */}
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image src="https://links.papareact.com/qd3"
-                    width={180}
-                    height={90}
+                    width={130}
+                    height={100}
                     alt="Picture of the author"
                 />
             </div>
 
             {/* moddle */}
             <div>
-
+                <input type='text' placeholder='texto'/>
             </div>
 
             {/* right */}
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
-import Image from 'next/image'
-import Header from '../../components/Header'
+import './globals.css'
+import Header from './components/Header'
 
 export default function Home() {
   return (
-    <div className=''>
-      <head>
-        <title>Create Next App By Me</title>
-        <link  rel='icon' href='/favicon.ico'/>
-      </head>
+    <>
       <Header/>
-    </div>
+    </>
   )
 }
