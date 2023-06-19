cb5b3def16c5da2c33ff3096ee1e1e5542083ea4
manuel-hg <manuel_hg_1@outlook.com>
2023-06-18 22:12:56 -0600

    update

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -16,8 +16,13 @@ function Header() {
             </div>
 
             {/* moddle */}
-            <div>
-                <input type='text' placeholder='texto'/>
+            <div className='flex items-center border-2 rounded-full p-2 justify-between shadow-sm'>
+                <input className='flex-grow pl-5 transparent outline-none text-gray-600 placeholder-gray-400' type='text' placeholder='Start your seach'/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-auto">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+
+                
             </div>
 
             {/* right */}
