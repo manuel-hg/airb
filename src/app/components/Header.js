bd8bd560b5c2210d54d7d389138f2712628e285b
manuel-hg <manuel_hg_1@outlook.com>
2023-06-20 23:05:57 -0600

    update avance

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -18,16 +18,27 @@ function Header() {
             {/* moddle */}
             <div className='flex items-center border-2 rounded-full p-2 justify-between shadow-sm'>
                 <input className='flex-grow pl-5 transparent outline-none text-gray-600 placeholder-gray-400' type='text' placeholder='Start your seach'/>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-auto">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                </svg>
-
-                
+                </svg>               
             </div>
 
             {/* right */}
-            <div>
+            <div className='flex space-x-4 items-center justify-center text-gray-400'>
+                <p className='cursor-pointer invisible md:visible'>Become a host</p>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                </svg>
+                <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white bg-gray-400 rounded-full">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
 
+
+                </div>
             </div>
         </header>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
 import './globals.css'
 import Header from './components/Header'
+import Banner from './components/Banner'
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Banner/>
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+        </section>
+      </main>
     </>
   )
 }
